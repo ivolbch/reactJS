c0d17b7950bb56d78b7eaf7d3b9088471d3e8afb
Ivo Medrano <ivolbch@hotmail.com>
2021-11-09 17:55:04 -0300

    UN1 TP Opt

--- script.js
+++
@@ -1,9 +0,0 @@
-function comprobarClave(){
-  clave1 = document.f1.clave1.value
-  clave2 = document.f1.clave2.value
-
-  if (clave1 == clave2)
-     alert("Las dos claves son iguales...\nRealizaríamos las acciones del caso positivo")
-  else
-     alert("Las dos claves son distintas...\nRealizaríamos las acciones del caso negativo")
-}
